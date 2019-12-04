bb4407f9c773dff4a873f1d33f68528fc75abcf5
gomcarter <gomcarter@vip.qq.com>
2019-12-04 08:52:30 +0800

    1.0.0 release

--- frameworks/xmlexcel/src/main/java/com/gomcarter/frameworks/xmlexcel/download/StorageType.java
+++ frameworks/xmlexcel/src/main/java/com/gomcarter/frameworks/xmlexcel/download/StorageType.java
@@ -1,28 +1,28 @@
-package com.gomcarter.frameworks.xmlexcel.download;
-
-import com.gomcarter.frameworks.base.common.NacosClientUtils;
-
-public enum StorageType {
-    /**
-     * 文件存放本地的一个文件目录下，nfs挂载此目录，即可在公网上下载
-     */
-    nfs {
-        @Override
-        public String getDownloadUrl(Downloader downloader, String fileName) {
-            // FIXME
-            return NacosClientUtils.getConfigAsString("nfs", "STORAGE_TYPE") + fileName;
-        }
-    },
-    /**
-     * 放到 oss
-     */
-    oss {
-        @Override
-        public String getDownloadUrl(Downloader downloader, String fileName) {
-            // FIXME
-            return null;
-        }
-    };
-
-    public abstract String getDownloadUrl(Downloader downloader, String fileName);
-}
+//package com.gomcarter.frameworks.xmlexcel.download;
+//
+//import com.gomcarter.frameworks.base.common.NacosClientUtils;
+//
+//public enum StorageType {
+//    /**
+//     * 文件存放本地的一个文件目录下，nfs挂载此目录，即可在公网上下载
+//     */
+//    nfs {
+//        @Override
+//        public String getDownloadUrl(Downloader downloader, String fileName) {
+//            // FIXME
+//            return NacosClientUtils.getConfigAsString("nfs", "STORAGE_TYPE") + fileName;
+//        }
+//    },
+//    /**
+//     * 放到 oss
+//     */
+//    oss {
+//        @Override
+//        public String getDownloadUrl(Downloader downloader, String fileName) {
+//            // FIXME
+//            return null;
+//        }
+//    };
+//
+//    public abstract String getDownloadUrl(Downloader downloader, String fileName);
+//}
