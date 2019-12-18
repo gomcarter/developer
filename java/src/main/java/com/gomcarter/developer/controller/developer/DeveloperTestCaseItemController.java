a60967b6a94dd24467e341ae6c1b73dd39a4c465
361878584@qq.com <zhangchao>
2019-12-18 17:41:27 +0800

    用例接口 提交

---
+++ java/src/main/java/com/gomcarter/developer/controller/developer/DeveloperTestCaseItemController.java
@@ -0,0 +1,78 @@
+package com.gomcarter.developer.controller.developer;
+
+import com.gomcarter.developer.entity.TestCaseItem;
+import com.gomcarter.developer.params.TestCaseItemParam;
+import com.gomcarter.developer.service.TestCaseItemService;
+import com.gomcarter.frameworks.base.pager.DefaultPager;
+import com.gomcarter.frameworks.interfaces.annotation.Notes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * @author gomcarter on 2019-12-18 16:13:58
+ */
+@RestController
+@RequestMapping("testCaseItem")
+public class DeveloperTestCaseItemController {
+
+    @Autowired
+    private TestCaseItemService testCaseItemService;
+
+    @GetMapping(value = "list", name = "获取接口用例地址列表")
+    List<TestCaseItem> list(TestCaseItemParam params, DefaultPager pager) {
+        return this.testCaseItemService.query(params, pager);
+    }
+
+    @GetMapping(value = "count", name = "获取接口用例地址总数")
+    Integer count(TestCaseItemParam params) {
+        return this.testCaseItemService.count(params);
+    }
+
+    @PostMapping(value = "", name = "新增接口用例")
+    public void insert(@Notes("接口用例名称") @RequestParam String name,
+                       @Notes("用户id") @RequestParam String resultHandler,
+                       @Notes("用户id") @RequestParam String config,
+                       @Notes("接口外键") @RequestParam Long fkInterfacesId,
+                       @Notes("接口用例外键") @RequestParam Long fkTestCaseId,
+                       @Notes("排序") @RequestParam Integer sort,
+                       @Notes("入参") @RequestParam String parmConfig) {
+        testCaseItemService.insert(new TestCaseItem()
+                .setName(name)
+                .setResultHandler(resultHandler)
+                .setConfig(config)
+                .setFkInterfacesId(fkInterfacesId)
+                .setFkTestCaseId(fkTestCaseId)
+                .setSort(sort)
+                .setParmConfig(parmConfig)
+        );
+    }
+
+    @PutMapping(value = "{id}", name = "修改接口用例")
+    public void update(@Notes("主键") @PathVariable("id") Long id,
+                       @Notes("接口用例名称") @RequestParam String name,
+                       @Notes("用户id") @RequestParam String resultHandler,
+                       @Notes("用户id") @RequestParam String config,
+                       @Notes("接口外键") @RequestParam Long fkInterfacesId,
+                       @Notes("接口用例外键") @RequestParam Long fkTestCaseId,
+                       @Notes("排序") @RequestParam Integer sort,
+                       @Notes("入参") @RequestParam String parmConfig) {
+        testCaseItemService.update(new TestCaseItem()
+                .setId(id)
+                .setName(name)
+                .setResultHandler(resultHandler)
+                .setConfig(config)
+                .setFkInterfacesId(fkInterfacesId)
+                .setFkTestCaseId(fkTestCaseId)
+                .setSort(sort)
+                .setParmConfig(parmConfig)
+        );
+    }
+
+    @GetMapping(value = "{id}", name = "获取接口用例详情")
+    public TestCaseItem get(@Notes("主键") @PathVariable("id") Long id) {
+        return this.list(new TestCaseItemParam().setId(id), new DefaultPager()).get(0);
+    }
+
+}
--- java/src/main/java/com/gomcarter/developer/params/TestCaseItemParam.java
+++ java/src/main/java/com/gomcarter/developer/params/TestCaseItemParam.java
@@ -1,69 +1,61 @@
-package com.gomcarter.developer.entity;
+package com.gomcarter.developer.params;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.gomcarter.frameworks.interfaces.annotation.Notes;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import java.util.Date;
 
 /**
- * @author gomcarter on 2019-12-18 16:34:04
+ * @author gomcarter on 2019-12-18 16:13:58
  */
 @Data
 @Accessors(chain = true)
-public class TestCaseItem {
+public class TestCaseItemParam {
 
     /**
      * 主键
      */
-    @TableId(type = IdType.AUTO)
+    @Notes("主键")
     private Long id;
-
 
     /**
      * 具体接口
      */
+    @Notes("具体接口")
     private String name;
     /**
      * 结果处理器，存储javascript脚本，此脚本应该return下一个接口的参数
      */
+    @Notes("结果处理器，存储javascript脚本，此脚本应该return下一个接口的参数")
     private String resultHandler;
     /**
      * 接口hash值
      */
+    @Notes("接口hash值")
     private String hash;
     /**
      * 配置
      */
+    @Notes("配置")
     private String config;
     /**
      * 接口外键
      */
+    @Notes("接口外键")
     private Long fkInterfacesId;
     /**
      * 接口用例外键
      */
+    @Notes("接口用例外键")
     private Long fkTestCaseId;
     /**
      * 
      */
+    @Notes("")
     private Date createTime;
     /**
      * 
      */
+    @Notes("")
     private Date modifyTime;
-    /**
-     * 排序
-     */
-    private Integer sort;
-    /**
-     * 入参
-     */
-    private String parmConfig;
-
-    //@NotReplaceableStart
-    // 重新生成代码时，NotReplaceableStart -> NotReplaceableEnd 中间的内容不会被覆盖
-
-
-    //@NotReplaceableEnd
 }
