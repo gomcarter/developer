f0fecd35140bc18311b5ab6497800db21d474b1c
gomcarter <gomcarter@vip.qq.com>
2020-03-03 13:04:58 +0800

    优化登录界面，新增长期登录功能

--- web/src/page/login/index.vue
+++ web/src/page/login/index.vue
@@ -5,16 +5,19 @@
       <p class="logo_title">开发者中心</p>
       <div class="form">
         <el-form ref="forms" :model="forms" label-width="0" :rules="rules">
-          <el-form-item prop="user">
-            <el-input v-model="forms.user" placeholder="请输入账号" @keypress.enter.native="onSubmit"></el-input>
+          <el-form-item prop="username">
+            <el-input v-model="forms.username" placeholder="请输入账号" @keypress.enter.native="onSubmit"></el-input>
           </el-form-item>
           <el-form-item prop="password">
             <el-input v-model="forms.password" type="password" placeholder="请输入密码" @keypress.enter.native="onSubmit"></el-input>
           </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="remember" style="float: left;">长期登录</el-checkbox>
+          </el-form-item>
         </el-form>
         <el-row>
         </el-row>
-        <el-button type="primary" @click="onSubmit" :disabled="disClick">{{disClick ? '登录中' : '登录'}}</el-button>
+        <el-button type="primary" @click="onSubmit" :disabled="disClick" :icon="disClick?'el-icon-loading':''">{{disClick ? '登录中' : '登录'}}</el-button>
       </div>
     </div>
   </div>
@@ -28,12 +31,13 @@ import { getUrlHashParams } from '@/config/utils'
 export default {
   data () {
     return {
+      remember: false,
       forms: {
-        user: '',
+        username: '',
         password: ''
       },
       rules: {
-        user: [
+        username: [
           { required: true, message: '请输入账号', trigger: 'blur' }
         ],
         password: [
@@ -45,13 +49,18 @@ export default {
   },
   computed: {},
   methods: {
-    onSubmit () { // 提交
+    onSubmit () {
       this.$refs.forms.validate((valid) => {
         if (valid) {
           this.disClick = true
           loginApi(this.forms).then((res) => {
-            // 设置过期时间: 10年
-            login(res)
+            let expire = 7200
+            if (this.remember) {
+              // 设置长期登录，过期时间: 10年
+              expire = 315360000
+            }
+            login(res, expire)
+            this.disClick = false
             if (this.redirect) {
               this.$router.push(decodeURIComponent(this.redirect))
             } else {
